79b83cbcbff2837788f12ee10cbbd8cf35621f4d
grant <grant@iowntheinter.net>
2024-02-26 00:20:06 -0700

    add script example

--- script/example-csv.sh
+++ script/example-csv.sh
@@ -4,6 +4,6 @@ java -jar graph-synth/target/GraphSynth-1.0.0-SNAPSHOT.jar\
   -d \
   -c conf/generator/generateToCSVFiles.properties \
   -s generator.schema.yaml.path=graph-synth/src/main/resources/gdemo_schema.yaml \
-  -s generator.scaleFactor=100000 \
-  -s runtime.threads=1 \
+  -s generator.scaleFactor=100 \
+  -s runtime.threads=3 \
   -s output.directory=/core/datasets/gdemo/gdemocsv1
